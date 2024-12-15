4c9058b3aaebd3e2b143c3e8d1ff14d306344abc
John <ngoctam.nguyen@edu.sait.ca>
2024-12-15 09:39:27 -0700

    Update connected db sql
    Update connected db sql

---
+++ app/restapi/part-1/api/dogs/[id]/route.js
@@ -0,0 +1,63 @@
+import { neon } from "@neondatabase/serverless";
+
+const dbURL = process.env.DATABASE_URL || "";
+const sql = neon(dbURL);
+
+export async function GET(request, {params} ) {
+    const {id} = await params;
+    const idNum= Number(id);
+    //retrieve the lists of dogs from the database
+    //select * from  dogs
+    const response = await sql`select * from dogs`;
+
+
+
+    let thisDog = response.find((dogs) => dogs.id == idNum );
+    //not exist 
+    if (!thisDog) {
+        return new Response("This dog does not exist", {status:404});
+    }
+    return new Response(JSON.stringify(thisDog), {status:200});
+}
+export async function PUT(request, {params}) {
+    const {id} = await params;
+    const idNum= Number(id);
+    let dog = await request.json();
+    //TODO: validation and error handling on dog object
+    // update dog number idnum in the database
+    const response = await sql`
+    UPDATE dogs SET name=${dog.name}, age=${dog.age} WHERE id=${idNum}
+    RETURNING *`;
+    //204 not data 200 have data
+    return new Response(JSON.stringify(response),{status:200});
+}
+//
+export async function PATCH(request, {params}) {
+    const {id} = await params;
+    const idNum= Number(id);
+    let dog = await request.json();
+    try {
+        if(dog.name){
+            //update the dog name in the database for dog number [idNum]
+            await sql`UPDATE dogs SET name = ${dog.name} WHERE id = ${idNum};`;
+        }
+        if(dog.age){
+            //update the dog age in the database for dog number
+            await sql`UPDATE dogs SET age = ${dog.age} WHERE id = ${idNum};`;
+        }
+    } catch (error) {
+        return new Response("Database Error",{status:503});
+    }
+    
+    return new Response(null,{status:204});
+}
+
+export async function DELETE(request, {params}) {
+    const {id} = await params;
+    const idNum= Number(id);
+    //check for authenticated user
+    //DELETE DOG NUMBER [IDNUM] FROM DATABASE
+    await sql`DELETE FROM dogs WHERE id = ${idNum}`;
+    return new Response(null,{status:204});
+
+}
--- app/restapi/part-1/page.js
+++ app/restapi/part-1/page.js
@@ -1,30 +1,85 @@
-export default async function ApiTestPage(){
+"use client"
+import { useEffect, useState } from "react";
 
-    let data="";
-    const response=await fetch("https://hinntam.vercel.app/restapi/part-1/api");
-
-    if(!response.ok){
-        data="Data could not be retrieved."
-
-    } else {
-        data=await response.text();
+export default function DogPage(){
+    
+    const [dogList,setDogList] = useState([]);
+    const [dogName,setDogName] = useState("");
+    const [dogAge,setDogAge] = useState(0); 
+    const handleChangeName = (event)=>{
+        setDogName(event.target.value);
     }
+    const handleChangeAge = (event)=>{
+        setDogAge(event.target.value);
+    }
+    async function handleSubmit(event){
+        event.preventDefault();
+        let newObjDog = {
+            name: dogName,
+            age: dogAge
+        }
+        console.log(JSON.stringify(newObjDog));
+        const request = new Request("http://localhost:3000/week-13/api/dogs",{
+            method:"POST",
+            body: JSON.stringify(newObjDog)
+        });
+        try {
+            const response = await fetch(request); 
+            if(response.ok) console.log("Success!");
+        } catch (error) {
+            console.log(error);
+        }
+        getAllDogs();
+    }
+    async function getAllDogs(){
+        try {
+            const response = await fetch("http://localhost:3000/week-13/api/dogs");
+            if(!response.ok) console.log(response.error);
+            const data = await response.json();
+            setDogList(data);
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+    async function deleteDog(idNum) {
+        const request = new Request(`http://localhost:3000/week-13/api/dogs/${idNum}`,{
+            method:"DELETE"
+        });
+        getAllDogs();
+    }
+    useEffect(() => {
+        getAllDogs();
+    },[]);
 
     return(
-        <main>
-            <h1>
-                Week 12 Part 1
-            </h1>
-            <p>{data}</p>
-            <form method="POST" action="http://localhost:3000/week-12/part-1/api">
+        <main className="p-5">
+            <header>
+            <h1>Dog Control Panel</h1>
+            
+            </header>
+            <section>
+                {
+                    dogList.map((dog) =>(
+                        <div key={dog.id} className="border-b-2">
+                            <p>Name:{dog.name}</p>
+                            <p>Age:{dog.age}</p>
+                        </div>
+                    ))
+                }
+            </section>
+            <form className="p-3 bg-slate-300" onSubmit={handleSubmit}>
+                <h2>Add New Dog</h2>
                 <div>
-                    <label>
-                        Test input:
-                    </label>
-                    <input type="text" name="test_input"/>
+                    <label>Name:</label>
+                    <input type="text" onChange={handleChangeName} value={dogName}/>
                 </div>
                 <div>
-                    <button type="submit">Submit</button>
+                    <label>Age:</label>
+                    <input type="text" onChange={handleChangeAge} value={dogAge}/>
+                </div>
+                <div>
+                    <button type="submit">Add dog</button>
                 </div>
             </form>
         </main>
