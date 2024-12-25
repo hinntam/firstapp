4344affc69588b69d07d285b7f9dbe55605823d5
John <ngoctam.nguyen@edu.sait.ca>
2024-12-25 13:04:54 -0700

    Updated link
    Updated link

--- app/dog-api/random/page.js
+++ app/dog-api/random/page.js
@@ -53,15 +53,20 @@ export default function PageRandomDogsAPI() {
                 <div className="container mx-auto px-4 py-8">
                     <h1 className="text-3xl font-bold mb-4 mt-5">End-point API dogs</h1>
                     <ul className="grid grid-cols-1 md:grid-cols-5 gap-4">
-                        <li className="border-b-2 border-blue-400 pb-2">
+                    <li className="border-b-2 border-blue-400 pb-2">
                             <Link href="/dog-api/all">List all of dogs</Link>
                         </li>
                         <li className="border-b-2 border-blue-400 pb-2">
-                            List dogs by keyword
+                            <Link href="/dog-api/keyword">List dogs by keyword</Link>
+                            
                         </li>
                         <li className="border-b-2 border-blue-400 pb-2">List category dogs</li>
-                        <li className="border-b-2 border-blue-400 pb-2">Detail of dog by ID</li>
-                        <li className="border-b-2 border-blue-400 pb-2">Random 5 dogs</li>
+                        <li className="border-b-2 border-blue-400 pb-2">
+                            <Link href="/dog-api/">Detail of dog by ID</Link>
+                        </li>
+                        <li className="border-b-2 border-blue-400 pb-2">
+                            <Link href="/dog-api/random">Random 5 dogs</Link>
+                        </li>
                     </ul>
                     
 
