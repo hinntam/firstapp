5d0324088889abf6d59ea13bd20e116532da8ca7
John <ngoctam.nguyen@edu.sait.ca>
2025-01-12 11:47:17 -0700

    updated project
    updated project

--- app/components/Project.js
+++ app/components/Project.js
@@ -6,6 +6,33 @@ import VideoPlayer from "./Video";
 // components/Projects.js
 const projects = [
   {
+    title: 'Alberta Driving Class 7',
+    description: 'The Alberta Driving Class 7 is a mobile application that helps users prepare for the Class 7 knowledge test. The application is developed using JavaScript, React, and Tailwind CSS for styling. The project leverages a mix of languages including HTML, CSS, and JavaScript.',
+    technologies: ['React native CLI', 'NodeJS', 'RestfullAPI','MongoDB','Express','Kotlin'],
+    image: '/assets/movies.png', // Replace with your project screenshot
+    demoLink: 'https://play.google.com/store/apps/details?id=com.codewithnguyen.todoalbertadriving&hl=en',
+    codeLink: 'https://play.google.com/store/apps/details?id=com.codewithnguyen.todoalbertadriving&hl=en',
+    video:'/assets/albertadriving.mp4'
+  },
+  {
+    title: 'How to cook',
+    description: 'Instruction how to cook food with ingredients and steps.',
+    technologies: ['React native CLI', 'OpenAPI','Kotlin'],
+    image: '/assets/movies.png', // Replace with your project screenshot
+    demoLink: 'https://play.google.com/store/apps/details?id=com.codewithnguyen.todogreenfood&hl=en',
+    codeLink: 'https://play.google.com/store/apps/details?id=com.codewithnguyen.todogreenfood&hl=en',
+    video:'/assets/howtocook.mp4'
+  },
+  {
+    title: '600 GPLX APP',
+    description: 'Review drivers license 600 with questions you can easily mistake and 60 points listed for use.',
+    technologies: ['React native CLI', 'OpenAPI','Kotlin','NodeJS','MongoDB','Express'],
+    image: '/assets/movies.png', // Replace with your project screenshot
+    demoLink: 'https://play.google.com/store/apps/details?id=com.codewithnguyen.tododriver&hl=en',
+    codeLink: 'https://play.google.com/store/apps/details?id=com.codewithnguyen.tododriver&hl=en',
+    video:'/assets/gplx600.mp4'
+  },
+  {
     title: 'Movies search App',
     description: 'The WhatToWatch allows to search movies through the keywords. The results displayed 5 movies random',
     technologies: ['Next.js', 'Tailwind CSS', 'JavaScript','API movies'],
@@ -19,8 +46,8 @@ const projects = [
     description: 'An application help to take care plant.',
     technologies: ['React Native', 'Node.js', 'MongoDB', 'Express'],
     image: '/assets/albertadrivingtest.png',
-    demoLink: 'https://play.google.com/store/apps/details?id=com.codewithnguyen.todoalbertadriving',
-    codeLink: 'https://github.com/hinntam/AlbertaClass7',
+    demoLink: '#',
+    codeLink: '#',
     video:'/assets/sprout.mp4'
   },
   {
